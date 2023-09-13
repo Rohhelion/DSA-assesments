b094e9798c2a7a5a30c7ea977dcc43e0762e7c5c
Rohhelion <133342455+Rohhelion@users.noreply.github.com>
2023-09-13 17:45:54 +0300

    Update is_balanced.py

--- is_balanced.py
+++ is_balanced.py
@@ -1,1 +1,22 @@
+def is_balanced(expression):
+    stack = []
+    opening_brackets = "([{"
+    closing_brackets = ")]}"
+    
+    for char in expression:
+        if char in opening_brackets:
+            stack.append(char)
+        elif char in closing_brackets:
+            if not stack:
+                return False  # Unmatched closing bracket
+            top = stack.pop()
+            if opening_brackets.index(top) != closing_brackets.index(char):
+                return False  # Mismatched brackets
+    return not stack  # True if the stack is empty
+
+# Test cases
+expression1 = "([]{})"
+expression2 = "([)]"
+print(is_balanced(expression1))  # Output True
+print(is_balanced(expression2))  # Output  False
 
