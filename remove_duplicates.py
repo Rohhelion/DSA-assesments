4cc9a5df7c3634e7d069365b34310797521e247a
Rohhelion <133342455+Rohhelion@users.noreply.github.com>
2023-09-13 17:47:39 +0300

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,1 +1,16 @@
+def remove_duplicates(sequence):
+    seen = set()
+    result = []
+    
+    for item in sequence:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    
+    return result
+
+# Test case
+input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+result = remove_duplicates(input_sequence)
+print(result)  # the output will remove the repeated numbers as follows [2, 3, 4, 5, 6, 7]
 
