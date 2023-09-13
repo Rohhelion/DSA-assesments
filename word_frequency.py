80350bfa7220291db956d2e92a0e3415d910efb3
Rohhelion <133342455+Rohhelion@users.noreply.github.com>
2023-09-13 17:48:47 +0300

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -1,1 +1,19 @@
+import re
+
+def word_frequency(sentence):
+    words = re.findall(r'\w+', sentence.lower())
+    word_count = {}
+    
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    
+    return word_count
+
+# Test case
+sentence = "This is a test sentence. This sentence is a test."
+result = word_frequency(sentence)
+print(result)
 
